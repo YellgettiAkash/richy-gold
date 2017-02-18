80b5b1d51bbdb43b4813b2778afefd956528c52c
Akash Yellgetti <akash050792@gmail.com>
2017-02-18 23:43:58 +0530

    database and contents added to the respective page

---
+++ wp-content/themes/richy-gold/header.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Richygold and Goldmoore</title>
+</head>
+<body>
---
+++ wp-content/themes/richy-gold/index.php
@@ -0,0 +1,12 @@
+<?php get_header()  ?>
+
+
+<?php
+			/* translators: %s: Name of current post */
+			the_content( sprintf(
+				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+				get_the_title()
+			) );
+
+			?>
+<?php get_footer()  ?>
